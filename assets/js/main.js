214a0ee36adc44cff9a4eb4d19d8d100a614f209
Sameer B <sameerflyleaf@gmail.com>
2022-09-09 15:52:05 -0700

    save button works

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
 
 $("#currentDay").text(moment().format("MMM, DD, YYYY"));
-
+$(".saveBtn").on("click", function() {
+    var activity = $(this).siblings(".decription").val();
+    console.log(activity);
+    var timeBlock = $(this).parent().attr("id");
+    localStorage.setItem(timeBlock, activity);
+})
 });
